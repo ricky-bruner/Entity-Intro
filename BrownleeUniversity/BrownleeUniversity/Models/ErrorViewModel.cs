92c5ba710e45cfdf56b4cc91c55b49a2c39e8e90
Ricky Bruner <ricky.bruner@gmail.com>
2018-11-26 14:34:33 -0600

    initial files

---
+++ BrownleeUniversity/BrownleeUniversity/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace BrownleeUniversity.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
